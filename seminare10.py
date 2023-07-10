3b69312d61bdf8d8914307818996ceea07ea41f7
Алексей <alekseybazanov@mail.ru>
2023-07-10 18:03:21 +0300

    first commit

---
+++ seminare10.py
@@ -0,0 +1,10 @@
+import random
+import pandas as pd
+
+lst = ['robot'] * 10
+lst += ['human'] * 10
+random.shuffle(lst)
+data = pd.DataFrame({'whoAmI': lst})
+
+one_hot_data = pd.get_dummies(data['whoAmI'])
+one_hot_data.head()
